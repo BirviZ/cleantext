83418cac490c51c87b9694e442b080ae94543eb7
birviz <birvizkozak@gmail.com>
2016-11-06 10:58:50 +0200

    Create file objects

--- src/org/birviz/cleantext/Main.java
+++ src/org/birviz/cleantext/Main.java
@@ -10,5 +10,29 @@ public class Main {
         Text text = new Text(test.get());
 
         test.send(text.toString());
+
+        File files[] = new File[args.length];
+
+        for (int i = 0; i < args.length; i++) {
+            files[i] = new File(new IO(true, args[i]), new Text(new IO(true, args[i]).get()));
+        }
+    }
+
+    private static class File {
+        private IO file;
+        private Text text;
+
+        public File(IO file, Text text) {
+            this.file = file;
+            this.text = text;
+        }
+
+        public IO getFile() {
+            return file;
+        }
+
+        public Text getText() {
+            return text;
+        }
     }
 }
